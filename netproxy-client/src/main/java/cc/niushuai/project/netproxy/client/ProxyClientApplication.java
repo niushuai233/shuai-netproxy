9470551766557ea5b3ec7470ce9697cd0bd3e044
niushuai233 <niushuai951101@gmail.com>
2022-04-23 20:15:01 +0800

    fix: 增加启用异步注解

--- netproxy-client/src/main/java/cc/niushuai/project/netproxy/client/ProxyClientApplication.java
+++ netproxy-client/src/main/java/cc/niushuai/project/netproxy/client/ProxyClientApplication.java
@@ -2,6 +2,7 @@ package cc.niushuai.project.netproxy.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 /**
  * 代理客户端
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author niushuai
  * @date 2022/4/22 13:18
  */
+@EnableAsync
 @SpringBootApplication
 public class ProxyClientApplication {
 
